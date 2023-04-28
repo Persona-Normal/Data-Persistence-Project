cbff4064e99ab82d2c684433b9dca679bc88309e
Persona-Normal <71715676+Persona-Normal@users.noreply.github.com>
2023-04-28 12:23:11 +0200

    Scripts Persistencia de datos y manejo de menu

--- Assets/Scripts/DataPersistenceScripts/DataPersistenceManager.cs
+++ Assets/Scripts/DataPersistenceScripts/DataPersistenceManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HighScoreManager : MonoBehaviour
+public class DataPersistenceManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
